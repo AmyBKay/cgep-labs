611a303b70f02d6e64ae39d2df01812abe5141e1
Amy Kalu <amaraben@yahoo.com>
2026-06-29 22:41:06 -0400

    fix: restore encryption config for green PR

--- terraform/primitives/compliant-s3/outputs.tf
+++ terraform/primitives/compliant-s3/outputs.tf
@@ -1,5 +1,5 @@
 output "bucket_arn"  { value = aws_s3_bucket.vault.arn }
 output "bucket_name" { value = aws_s3_bucket.vault.id }
-#output "encryption_algorithm" {
-#  value = tolist(aws_s3_bucket_server_side_encryption_configuration.vault.rule)[0].apply_server_side_encryption_by_default[0].sse_algorithm
-#}
+output "encryption_algorithm" {
+  value = tolist(aws_s3_bucket_server_side_encryption_configuration.vault.rule)[0].apply_server_side_encryption_by_default[0].sse_algorithm
+}
